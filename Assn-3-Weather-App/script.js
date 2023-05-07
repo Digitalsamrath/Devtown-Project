07c510e014fed534017e128149b500eb33a39a80
Samrath Thapar <42505185+Digitalsamrath@users.noreply.github.com>
2023-05-07 13:58:29 +0530

    script.js

--- Assn-3-Weather-App/script.js
+++ Assn-3-Weather-App/script.js
@@ -1,6 +1,4 @@
-// JavaScript code for the Weather Search Application
 
-// Get DOM elements
 const locationPrompt = document.getElementById('location-prompt');
 const allowLocationBtn = document.getElementById('allow-location');
 const locationError = document.getElementById('location-error');
@@ -11,22 +9,22 @@ const locationName = document.getElementById('location-name');
 const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
 
-// Check if geolocation is supported by the browser
+
 if ('geolocation' in navigator) {
-  // Display location access prompt
+  
   locationPrompt.style.display = 'block';
 
-  // Event listener for "Allow Location Access" button
+  
   allowLocationBtn.addEventListener('click', () => {
-    // Get user's current location
+   
     navigator.geolocation.getCurrentPosition(getWeatherByCoordinates, handleLocationError);
   });
 } else {
-  // Geolocation is not supported
+  
   locationError.style.display = 'block';
 }
 
-// Event listener for search form submission
+
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = searchInput.value.trim();
@@ -35,16 +33,15 @@ searchForm.addEventListener('submit', (e) => {
   }
 });
 
-// Function to handle location access error
+
 function handleLocationError(error) {
   locationPrompt.style.display = 'none';
   locationError.style.display = 'block';
 }
 
-// Function to get weather data by coordinates
 function getWeatherByCoordinates(position) {
   const { latitude, longitude } = position.coords;
-  const apiKey = 'f3edf7a821749de49e9ac26c6df79cfa'; // Replace with your OpenWeatherMap API key
+  const apiKey = 'f3edf7a821749de49e9ac26c6df79cfa'; 
   const apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
   fetch(apiEndpoint)
@@ -57,9 +54,9 @@ function getWeatherByCoordinates(position) {
     });
 }
 
-// Function to get weather data by location name
+
 function getWeatherByLocation(location) {
-  const apiKey = 'f3edf7a821749de49e9ac26c6df79cfa'; // Replace with your OpenWeatherMap API key
+  const apiKey = 'f3edf7a821749de49e9ac26c6df79cfa'; 
   const apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
 
   fetch(apiEndpoint)
@@ -72,13 +69,13 @@ function getWeatherByLocation(location) {
     });
 }
 
-// Function to display weather data
+
 function displayWeatherData(data) {
   const name = data.name;
-  const temp = Math.round(data.main.temp - 273.15); // Convert temperature from Kelvin to Celsius
+  const temp = Math.round(data.main.temp - 273.15); 
   const desc = data.weather[0].description;
 
-  // Display weather data
+
   locationPrompt.style.display = 'none';
   weatherDisplay.style.display = 'block';
   locationName.textContent = name;
